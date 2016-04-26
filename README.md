3fbda12247282cfaac0a5f39ba416231e668816c
kcp288 <kira.prentice@nyu.edu>
2016-04-26 10:28:42 -0400

    error handling and print statements. building more info into output

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 ##### Copula presence/absence counter
 African American English (Spring 2016), Professor Renée Blake
 
+##### COPULA DELETION FEATURES
+IS | ARE 
+AFTER PRONOUN | NOUN
+BEFORE ADJECTIVE | NOUN | GONNA/GON/GOING TO 
+
 ###TO-DO
 
 This checks for PRP + VBZ, but VBZ only applies to 'is'. Going to hardcode copular instances ('is', 'are') as well as exclude cases of pronoun before non-copular situations (PRP + IN, for example). 
--- copula.py
+++ copula.py
@@ -1,28 +1,106 @@
 import nltk
 
-def copula(user_in):
-  copula = 0
-  copula_null = 0
+copula_list = []
 
-  sentence = user_in
+def get_pos(token):
+  word = token[0]
+  pos = token[1]
+  return word, pos
+
+def print_copula(copula_list):
+  total = len(copula_list)
+  present = 0
+  absent = 0
+
+  for i in copula_list:
+    if (i[0] == 0):
+      present = present + 1
+    if (i[0] == 1):
+      absent = absent + 1
+
+  present_per = float(present)/total
+  absent_per = float(absent)/total
+
+  print 'Total copula spaces:\t' + str(total) + '\n' + \
+  'Present:\t' + str(present) + '/' + str(total) + '\t (' + str(present_per) + '% of total)\n' + \
+  'Absent:\t' + str(absent) + '/' + str(total) + '\t (' + str(absent_per) + '% of total)\n'
+
+def copula(user_in):
+  
+  
+
+  # 0: 0 - present, 1 - absent
+  # 1: 0 - 'is', 1 - 'are'
+  # 2: 0 - after 'PRP', 1 - after NOUN
+  # 3: 0 - before 'JJ', 1 - before NOUN, 2 - before GONNA
+
+
+  # COPULA DELETION FEATURES
+  # IS | ARE 
+  # AFTER PRONOUN | NOUN
+  # BEFORE ADJECTIVE | NOUN | GONNA/GON/GOING TO 
+
+  sentence = user_in.upper()
   tokens = nltk.word_tokenize(sentence)
   tagged = nltk.pos_tag(tokens)
 
   for i,tup in enumerate(tagged):
-    word = tup[0]
-    pos = tup[1]
-    if (pos == 'PRP'):
-      print "Pronoun"
-      if tagged[i+1]:    
-        next = tagged[i+1]
-        next_word = next[0]
-        next_pos = next[1]
-        if (next_pos == 'VBZ'):
-          copula = copula + 1
-        else:
-          copula_null = copula_null + 1
+    try:
+      copula = [0,0,0,0]
+
+      word, pos = get_pos(tup)
+
+      # Eliminate cases
+        # If before '.' (not sentence final)
+        # Not contracted 's
+        # Not first person "am"
+        # Not infinitive 'be'
+        # Not ain't
+      '''
+      if (pos == 'PRP') or (pos == "NN") or (pos == "NNS") or (pos == "NNP"):
+        # Not sentence final
+        # TODO: Check period
+        if tagged[i+1]:
+          # Check first person
+          if tagged[i+1] == 'AM': 
+            print "First person"
+            break
+          else:
+            continue
+        else: 
+          print "Word final"
+          break
+      '''
+
+      # Following a PRONOUN
+      if (pos == 'PRP'):
+        # Make su
+        if tagged[i+1]:    
+          next_word, next_pos = get_pos(tagged[i+1])
+          fol_word, fol_pos = get_pos(tagged[i+2])
+          if (next_word == 'IS') or (next_word == 'ARE'):
+            copula[0] = 0
+
+          else:
+            copula[0] = 1
+
+      # Following a NOUN (less likely) 
+      if (pos == "NN") or (pos == "NNS") or (pos == "NNP"):
+        if tagged[i+1]:    
+          next_word, next_pos = get_pos(tagged[i+1])
+          fol_word, fol_pos = get_pos(tagged[i+2])
+          if (next_word == 'IS') or (next_word == 'ARE'):
+            copula[0] = 0
+          else:
+            copula[0] = 1
+
+      copula_list.append(copula)
+    except IndexError:
+      pass
+  # print tagged
+  # print "Copula presence: " + str(copula) + " | Copula absence: " + str(copula_null)
   print tagged
-  print "Copula presence: " + str(copula) + " | Copula absence: " + str(copula_null)
+  print_copula(copula_list)
 
 def main():
   cont = True
