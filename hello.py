78cc7b056e55f531bbc04af85938ec75a19d4089
kcp288 <kira.prentice@nyu.edu>
2016-05-08 22:54:18 -0400

    lol i hate heroku

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import copula
+import os
 
 app = Flask(__name__)
 
@@ -42,5 +43,6 @@ def example():
 	return render_template('example.html')
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get("PORT",5000))
+	app.run(host="0.0.0.0", port=port)
 
